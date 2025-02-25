88c187971e7b1b1d0590634d89dc31112b58ab34
aashovkin <artemshshovkin@yandex.ru>
2025-02-25 13:12:18 +0300

    fixed url to manifest

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import TonConnectUIProvider from './external/TonConnectUIProvider.tsx'
 
-const manifestUrl = 'https://25x8.github.com/first_ton_app_frontend/manifest.json';
-
+const manifestUrl = 'https://raw.githubusercontent.com/25x8/first_ton_app_frontend/master/public/manifest.json';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TonConnectUIProvider manifestUrl={manifestUrl}>
